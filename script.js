e7ee45f6a0f615951a54b1b7b5419013e76a5e4c
SSchulze4 <skschulz@ksu.edu>
2025-12-11 20:51:57 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 document.getElementById('colorBtn').onclick = function () {
-  const box = document.getElementById('homeContainer');
-  const current = getComputedStyle(box).backgroundColor;
+  const body = document.body;
+  const current = getComputedStyle(body).backgroundColor;
   
-  box.style.backgroundColor =
-    current.includes('0, 123, 255') ? '#28a745' : '#007bff';
+  body.style.backgroundColor =
+    current.includes("33, 35, 37") ? "black" : "rgba(33, 35, 37, 0.645)";
 };
