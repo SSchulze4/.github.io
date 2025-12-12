e52de72e42e6cee111829a509c1b25bf21517288
SSchulze4 <skschulz@ksu.edu>
2025-12-11 18:20:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-  const colorBtn = document.getElementById('colorBtn');
-  const homeContainer = document.getElementById('homeContainer');
+document.addEventListener("DOMContentLoaded", () => {
+  const colorBtn = document.getElementById("colorBtn");
+  const homeContainer = document.getElementById("homeContainer");
 
   if (colorBtn && homeContainer) {
-    colorBtn.addEventListener('click', function () {
-     
-      const randomColor = `rgba(${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, ${Math.floor(Math.random()*256)}, 0.7)`;
-      homeContainer.style.backgroundColor = randomColor;
+    colorBtn.addEventListener("click", () => {
+      if (homeContainer.style.backgroundColor === "rgba(0, 122, 204, 0.7)") {
+        homeContainer.style.backgroundColor = "rgba(33, 35, 37, 0.645)";
+      } else {
+        homeContainer.style.backgroundColor = "rgba(0, 122, 204, 0.7)";
+      }
     });
   }
 });
