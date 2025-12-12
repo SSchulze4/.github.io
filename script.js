f0675c120ed3e2aa201e1d1203bc9a8a5301da17
SSchulze4 <skschulz@ksu.edu>
2025-12-11 21:57:13 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-document.getElementById('colorBtn').onclick = function () {
-  const body = document.body;
-  const current = getComputedStyle(body).backgroundColor;
-  
-  body.style.backgroundColor =
-    current.includes("33, 35, 37") ? "black" : "rgba(33, 35, 37, 0.645)";
+document.getElementById("colorBtn").onclick = function () {
+  const container = document.getElementById("homeContainer");
+
+  container.classList.add("fade-out");
+
+  setTimeout(() => {
+    container.classList.remove("fade-out");
+  }, 800);
 };
