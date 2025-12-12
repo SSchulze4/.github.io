13fe4ca40b9b98814abb239e2c1ac30e1a25b8f2
SSchulze4 <skschulz@ksu.edu>
2025-12-11 20:33:25 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,5 @@
-const homeSection = document.getElementById('homeContainer');
-const colorBtn = document.getElementById('colorBtn');
-
-if (homeSection && colorBtn) {
-  colorBtn.onclick = () => {
-    if (homeSection.style.backgroundColor === 'lightblue') {
-      homeSection.style.backgroundColor = 'rgba(33, 35, 37, 0.645)';
-    } else {
-      homeSection.style.backgroundColor = 'lightblue';
-    }
-  };
-}
+document.getElementById('colorBtn').onclick = function () {
+  const box = document.getElementById('homeContainer');
+  box.style.backgroundColor =
+    box.style.backgroundColor === 'rgb(0, 123, 255)' ? '#28a745' : '#007bff';
+};
